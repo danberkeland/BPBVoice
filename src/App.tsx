8909825d803751a495b8e558d4952a48a8581368
Dan Berkeland <danberkeland@gmail.com>
2022-01-23 09:25:12 -0800

    updated Auth

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import React, { useEffect} from "react";
 import { SpeechProvider } from "@speechly/react-client";
 import Amplify, { Auth } from "aws-amplify";
 import awsconfig from "./aws-exports";
-import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
-
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import '@aws-amplify/ui-react/styles.css';
 
 import "./App.css";
 
@@ -12,12 +12,20 @@ import { SpeechApp } from "./SpeechApp";
 Amplify.configure(awsconfig);
 Auth.configure(awsconfig);
 
-function App(): JSX.Element {
+interface AuthProps {
+  signOut: any,
+  user: any
+}
+
+function App(props:AuthProps): JSX.Element {
 
  
   return (
     <div className="App">
-      <AmplifySignOut />
+      <>
+      <h1>Hello {props.user.username}</h1>
+      <button onClick={props.signOut}>Sign out</button>
+    </>
       <SpeechProvider appId="685bb1db-a418-4ff1-9a0c-dfcc888e23ec" language="en-US">
         <SpeechApp />
       </SpeechProvider>
