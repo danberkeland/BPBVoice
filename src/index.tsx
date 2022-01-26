d0bd082a41edc14c73bf1cd762a21e91cd7a2c9b
Dan Berkeland <danberkeland@gmail.com>
2022-01-26 04:45:09 -0800

    aws.export cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -4,65 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Amplify, Auth }  from "aws-amplify";
-import awsconfig from "./aws-exports";
+import { awsconfig, AuthSetup, APISetup } from "./aws-exports";
 
 
-const AuthSetup = {
-  "userPoolId": "us-east-2_BsLDGfO0R",
-  "userPoolWebClientId": "4hhvnta9vje0k9bimhagcjtrob",
-  "region": "us-east-2",
-  "identityPoolId": "us-east-2:6862d50d-412d-432c-8d77-4867cef8fb94",
-  "identityPoolRegion": "us-east-2",
-  "mandatorySignIn": false,
-  "aws_project_region": "us-east-2",
-  "aws_appsync_graphqlEndpoint": "https://aceshl3krjg4rgtaxctab6f6je.appsync-api.us-east-2.amazonaws.com/graphql",
-  "aws_appsync_region": "us-east-2",
-  "aws_appsync_authenticationType": "API_KEY",
-  "aws_appsync_apiKey": "da2-qdistujpznfy5fl3u2onoimzmm",
-  "aws_cloud_logic_custom": [
-    {
-      "name": "grabinfo",
-      "endpoint": "https://tohuzsgp4b.execute-api.us-east-2.amazonaws.com/staging",
-      "region": "us-east-2"
-    },
-    {
-      "name": "testsend",
-      "endpoint": "https://lpw2pqz8b0.execute-api.us-east-2.amazonaws.com/staging",
-      "region": "us-east-2"
-    },
-    {
-      "name": "grabqbcust",
-      "endpoint": "https://kokhhg0fze.execute-api.us-east-2.amazonaws.com/staging",
-      "region": "us-east-2"
-    }
-  ],
-  "aws_cognito_identity_pool_id": "us-east-2:6862d50d-412d-432c-8d77-4867cef8fb94",
-  "aws_cognito_region": "us-east-2",
-  "aws_user_pools_id": "us-east-2_BsLDGfO0R",
-  "aws_user_pools_web_client_id": "4hhvnta9vje0k9bimhagcjtrob",
-  "oauth": {},
-  "Auth": {
-    "identityPoolId": "us-east-2:6862d50d-412d-432c-8d77-4867cef8fb94",
-    "region": "us-east-2"
-  }
-}
-
-const ampConfig = Amplify.configure({
+Amplify.configure({
   ...awsconfig,
   Auth: AuthSetup,
-  API: {
-    graphql_endpoint: "https://aceshl3krjg4rgtaxctab6f6je.appsync-api.us-east-2.amazonaws.com/graphql",
-    graphql_headers: async () => ({
-      'x-api-key': 'da2-qdistujpznfy5fl3u2onoimzmm'
-    })
-
-  }
+  API: APISetup
 })
 
-console.log("ampConfig", ampConfig)
-
-const authConfig = Auth.configure();
-console.log("authConfig",authConfig)
+Auth.configure();
 
 ReactDOM.render(
   <React.StrictMode>
