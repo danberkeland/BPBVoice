6323d3f42a83a286efcd3c2136e81d8da36765b7
Dan Berkeland <danberkeland@gmail.com>
2022-01-29 19:04:45 -0800

    route box styling

--- src/SpeechApp.tsx
+++ src/SpeechApp.tsx
@@ -48,7 +48,7 @@ const BasicContainer = styled.div`
 
 const FulfillOptions = styled.div`
   display: grid;
-  grid-template-columns: 1fr .25fr 1fr .25fr 1fr .25fr;
+  grid-template-columns: 1fr .5fr 1fr .5fr 1fr .5fr;
   margin: 2px;
   align-items: center;
   justify-items: right;
